9f39fe9e5950b337d925689a472318fbd5e1c751
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-26 01:48:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,9 +16,9 @@ include Memorable::InstanceMethods
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
+#  end
 
   def self.all
     @@artists
